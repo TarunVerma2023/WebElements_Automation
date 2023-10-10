7dbddaf559559577752879c060087bb838089788
TarunVerma2023 <145251808+TarunVerma2023@users.noreply.github.com>
2023-10-10 23:37:25 +0530

    Add files via upload

---
+++ DropDown.java
@@ -0,0 +1,13 @@
+package com.crio.webActions;
+
+public class DropDown {
+	
+	public void createSelectObject() {
+		// TODO : Create the object of select class
+		
+    //Select dropDown = new Select(dropDownWE); 
+	//WebElement dropDownWE = driver.findElement(By.id(“searchDropdownBox”));	
+    
+	}
+
+}
